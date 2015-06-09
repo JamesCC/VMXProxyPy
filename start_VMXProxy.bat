951be2d0c46394c30abb9d53c6bde034bfdb64be
James Covey-Crump <james.business@covey-crump.me.uk>
2015-06-09 19:19:04 +0100

    Remove version import in setup to avoid issues when installing

--- start_VMXProxy.bat
+++ start_VMXProxy.bat
@@ -1,4 +1,5 @@
 @echo off
+set DIST_PATH=dist
 
 if "%1"=="" goto noparams
 
@@ -6,17 +7,10 @@ echo Your IP address(es):
 ipconfig | findstr /R /b /C:" *IP.*"
 echo.
 
-reg.exe Query HKLM\Hardware\Description\System\CentralProcessor\0 | find /i "x86"
-if %ERRORLEVEL% == 0 (
-    set DIST_PATH=dist32
-) else (
-    set DIST_PATH=dist64
-)
-
 if exist %DIST_PATH%\NUL (
     %DIST_PATH%\VMXProxy %*
 ) ELSE (
     python VMXProxy %*
 )
 
-noparams:
+:noparams
